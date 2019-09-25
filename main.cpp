8b49de14ecdd390f1345bbc1ca34a909f694fb44
fsqx <1037259296@qq.com>
2019-09-25 23:03:20 +0800

    更新main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <cstdlib>
-#include "001.two_sum.cpp"
+#include "007_number_flip.cpp"
 
 using namespace std;
 
 int main()
 {
 	Solution solution;
-
+	cout << solution.reverse(-7763) << endl;
 	cout << "Hello World!" << endl;
 	system("pause");
 	return 0;
