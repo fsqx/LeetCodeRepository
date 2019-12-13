7ace0a8280cfe333fbbe08d0292e58931f27bc7a
fsqx <34276269+fsqx@users.noreply.github.com>
2019-12-13 11:20:27 +0800

    调试026
    调试026

--- main.cpp
+++ main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <cstdlib>
-#include "021_merge_two_sorted_lists.h"
+#include <vector>
+//#include "001_two_sum.h"
+//#include "007_number_flip.h"
+//#include "009_number_of_tracts.h"
+//#include "013_roman_to_interger.h"
+//#include "014_Longest_Common_Prefix.h"
+//#include "020_valid_parentheses.h"
+//#include "021_merge_two_sorted_lists.h"
+#include "026_remove_dumplicates_from_sorted_Array.h"
 
 using namespace std;
 
 int main()
 {
-    ListNode one(1);
-    ListNode two(2);
-    ListNode three(3);
-    one.next = &two;
-    two.next = &three;
-    ListNode four(1);
-    ListNode five(3);
-    ListNode six(4);
-    four.next = &five;
-    five.next = &six;
+    vector<int> nums(5, 0);
+    nums[1] = 1;
+    nums[2] = 1;
+    nums[3] = 4;
+    nums[4] = 4;
     Solution solution;
-    solution.mergeTwoLists(&one, &four);
+    cout << solution.removeDuplicates(nums) << endl;
     system("pause");
     return 0;
 }
