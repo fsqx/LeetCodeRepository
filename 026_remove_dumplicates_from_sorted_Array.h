64f3f66cc09edd8a31f3fb571be13cc251012c3b
fsqx <34276269+fsqx@users.noreply.github.com>
2019-12-13 11:20:16 +0800

    Create 026_remove_dumplicates_from_sorted_Array.h
    新增026解题

---
+++ 026_remove_dumplicates_from_sorted_Array.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int>& nums);
+
+};
+
+int Solution::removeDuplicates(vector<int>& nums)
+{
+    // ¿ØÊý×é
+    if(nums.size() == 0)
+    {
+        return 0;
+    }
+
+    auto iter = nums.begin();
+    int frontNum = nums.front();
+    bool hasIntMax = false;
+    iter++;
+
+    while(iter != nums.end())
+    {
+        if(*iter == INT_MAX)
+        {
+            hasIntMax = true;
+        }
+
+        if(*iter != frontNum)
+        {
+            frontNum = *iter;
+        }
+        else
+        {
+            *iter = INT_MAX;
+        }
+
+        iter++;
+    }
+
+    nums.erase(remove(nums.begin(), nums.end(), INT_MAX), nums.end());
+
+    if(hasIntMax)
+    {
+        nums.push_back(INT_MAX);
+    }
+
+    return nums.size();
+}
