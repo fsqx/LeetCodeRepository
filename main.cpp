b9e65d09dbf193d40cf34f3bdbce97b269c95829
fsqx <1037259296@qq.com>
2019-12-14 01:51:23 +0800

    update 028 strstr

--- main.cpp
+++ main.cpp
@@ -8,20 +8,16 @@
 //#include "014_Longest_Common_Prefix.h"
 //#include "020_valid_parentheses.h"
 //#include "021_merge_two_sorted_lists.h"
-#include "026_remove_dumplicates_from_sorted_Array.h"
+//#include "026_remove_dumplicates_from_sorted_Array.h"
 //#include "027_remove_element.h"
+#include "028_myStrstr.h"
 
 using namespace std;
 
 int main()
 {
-    vector<int> nums(5, 0);
-    nums[1] = 1;
-    nums[2] = 1;
-    nums[3] = 4;
-    nums[4] = 4;
     Solution solution;
-    cout << solution.removeDuplicates(nums) << endl;
+    cout << solution.strStr("", "") << endl;
     system("pause");
     return 0;
 }
