e3cec34c73c3e83c0b67e464481008007438b63f
wy <1037259296wy@gmail.com>
2020-03-30 00:52:18 +0800

    Create 035_search_insert_position.cpp

---
+++ 035_search_insert_position.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] >= target)
+            {
+                return i;
+            }
+        }
+        return nums.size();
+    }
+};
+
+int main()
+{
+    std::vector<int> nums;
+    nums.push_back(1);
+    nums.push_back(3);
+    nums.push_back(5);
+    nums.push_back(7);
+    Solution solution;
+    std::cout << solution.searchInsert(nums, 2) << std::endl;
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
