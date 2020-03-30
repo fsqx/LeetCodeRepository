3987babd6f6f77ebb682418fc9642961f3b4bd62
fsqx <1037259296@qq.com>
2020-03-30 16:02:38 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-
 #include "mainHead.h"
+#include "glog/logging.h"
+#define GOOGLE_GLOG_DLL_DECL
 
-int main()
+
+int main(int argc, char *argv[])
 {
 #if 0
     vector<int> nums(5, 0);
@@ -12,9 +14,12 @@ int main()
     Solution solution;
     cout << solution.removeDuplicates(nums) << endl;
 #endif
+    google::InitGoogleLogging(argv[0]);
+    //google::SetLogDestination(google::GLOG_INFO, "d:\\Logs\\INFO_");
     string ss = "abacbacabccbb";
     Solution solution;
     solution.longestPalindrome(ss);
     system("pause");
+    google::ShutdownGoogleLogging();
     return 0;
 }
