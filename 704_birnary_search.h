b2ed2934e36134ec7d42663b870bc4e51d53eab7
fsqx <1037259296@qq.com>
2020-04-22 00:47:21 +0800

    完善二分查找

--- 704_birnary_search.h
+++ 704_birnary_search.h
@@ -5,50 +5,69 @@
 
 using namespace std;
 
-class Solution
+/**
+ * 对于左右边界的二分法查找，重点在于明确查找区间，
+ * 当查找到元素时不是直接返回元素的索引，而是将该位置作为查找边界，直至查找区间为空
+ * 二分法查找应考虑是否越界，尤其是特殊二分法查找时
+ */
+
+class BirnarySearch
 {
 public:
-    Solution()
-    {
-        m_left = 0;
-        m_right = -2;
-    }
-
+    // 普通二分法查找
     int search(vector<int>& nums, int target)
     {
-        if(m_right == -2)
+        int length = nums.size();
+        int left = 0;
+        int right = length - 1;
+
+        while(left <= right)
         {
-            m_right = nums.size() - 1;
+            int mid = (right - left) / 2 + left;
+
+            if(nums.at(mid) > target)
+            {
+                right = mid - 1;
+            }
+            else if(nums.at(mid) < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
         }
 
-        if(m_left > m_right)
-        {
-            return -1;
-        }
-
-        int vctLength = m_right - m_left;
-        int mid = vctLength / 2 + m_left;
-
-        if(nums.at(mid) > target)
-        {
-            m_right = mid - 1;
-            return search(nums, target);
-        }
-        else if(nums.at(mid) < target)
-        {
-            m_left = mid + 1;
-            return search(nums, target);
-        }
-        else if(nums.at(mid) == target)
-        {
-            return mid;
-        }
-
-        return 0;
+        return -1;
     }
 
-private:
-    int m_left;
-    int m_right;
+    // 查找左边界的二分法查找 TODO
+    int searchLeft(vector<int>& nums, int target)
+    {
+        int length = nums.size();
+        int left = 0;
+        int right = length - 1;
+
+        while(left <= right)
+        {
+            int mid = (right - left) / 2 + left;
+
+            if(nums.at(mid) > target)
+            {
+                right = mid - 1;
+            }
+            else if(nums.at(mid) < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return -1;
+    }
 };
 #endif // !_BIRNARY_SEARCH_HPP_
