4535516760b21c80905b9a2f09edf5d2bc6b8494
fsqx <1037259296@qq.com>
2020-06-26 12:53:31 +0800

    Update mainHead.h

--- mainHead.h
+++ mainHead.h
@@ -5,22 +5,32 @@
 #include <algorithm>
 #include <chrono>
 #include <omp.h>
+
+/// ---------------------算法题---------------------
 //#include "001_two_sum.h"
 //#include "002_add_two_numbers.h"
 //#include "003_longest_substr.h"
 //#include "005_Longest_palindromic_sumstring.h"
+//#include "006_zigzag_conversion.h"
 //#include "007_number_flip.h"
 //#include "009_number_of_tracts.h"
 //#include "013_roman_to_interger.h"
 //#include "014_Longest_Common_Prefix.h"
-#include "015_three_sum.hpp"
+//#include "015_three_sum.hpp"
+//#include "019_remove_nth_node_from_end_of_list.h"
 //#include "020_valid_parentheses.h"
 //#include "021_merge_two_sorted_lists.h"
 //#include "026_remove_dumplicates_from_sorted_Array.h"
 //#include "027_remove_element.h"
-
-
-// birnarySearch
+#include "521_longest_uncommon_subsequence.h"
 //#include "704_birnary_search.h"
+
+
+/// ---------------------面试题---------------------
+//#include "01_08_zero_matrix.h"
+//#include "02_01_remove_duplicate_node.h"
+
+
+
 
 using namespace std;
