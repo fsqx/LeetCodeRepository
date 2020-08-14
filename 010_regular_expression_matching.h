c624bf0a86efb1fd383803021a11d1316516b813
fsqx <1037259296@qq.com>
2020-08-14 19:01:25 +0800

    Update 010_regular_expression_matching.h

--- 010_regular_expression_matching.h
+++ 010_regular_expression_matching.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <string>
 using namespace std;
 
@@ -14,14 +14,16 @@ public:
         int regExpLen = p.length();
         int regExpIndex = 0;
 
+        // 主动遍历正则
         for(int i = 0; i < regExpLen; i++)
         {
             matchChar = s.at(srcIndex);
             regChar = p.at(i);
 
+            // 正则是字母
             if((regChar >= 'a' && regChar <= 'z') || (regChar >= 'A' && regChar <= 'Z'))
             {
-                if(i != regExpLen - 1 && p.at(i) == '*')
+                if(i != regExpLen - 1 && p.at(i + 1) == '*')
                 {
                     char tmpMatchChar = s.at(srcIndex);
 
@@ -38,9 +40,37 @@ public:
                         }
                     }
                 }
+                else if(i != regExpLen - 1 && p.at(i + 1) != '*')
+                {
+                    char tmpMatchChar = s.at(srcIndex);
+
+                    if(tmpMatchChar == matchChar)
+                    {
+                        srcIndex++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            // 正则是*
+            else if((regChar == '.'))
+            {
+                if(i != regExpLen - 1 && p.at(i + 1) == '*')
+                {
+                    return true;
+                }
+                else
+                {
+                    srcIndex++;
+                }
             }
 
             char &reg = p.at(i);
         }
+
+        return true;
     }
+
 };
